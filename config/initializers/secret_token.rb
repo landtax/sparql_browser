91ae0b7691dea534ba2bfa4e9a28165e9a1cfeef
Santi <santiago.bel@webfordesigners.com>
2013-11-21 17:41:27 +0100

    rails 4.0.1 setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SparqlBrowser::Application.config.secret_key_base = '8c09d9548e06ba991f8bed4bbe78c4dc139f7c72dca9ff051a3b30fed04d344ad5114c3be761c932ff6ab9530cf28639cfa7e125f0bc942e14d6f8dfdef4f4aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SparqlBrowser::Application.config.session_store :cookie_store, key: '_sparql_browser_session'
