9452dc3be4f61c2e0ea7727815af3191e6b6135f
Santi <santiago.bel@webfordesigners.com>
2014-01-02 15:08:28 +0100

    document faceted

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < Resource::Base
 
   def self.find_all_by_facet(facet)
     normal_facet = facet.downcase
-    return [] unless ['subject', 'related_services'].include? normal_facet.downcase
+    return [] unless ['subject', 'related_services', 'related_resources', 'topic'].include? normal_facet.downcase
     solutions = self.send(:"find_all_faceted_by_#{normal_facet}")
     SolutionsBrowser.new(solutions)
   end
@@ -54,14 +54,14 @@ prefix dc:  <http://purl.org/dc/elements/1.1/>
 prefix dcterms:  <http://purl.org/dc/terms/>
 prefix ms: <http://gilmere.upf.edu/ms.ttl#>
 prefix bio: <http://gilmere.upf.edu/bio.ttl#>
-SELECT * {  ?page a owl:NamedIndividual
+SELECT * {  ?page_id a owl:NamedIndividual
     {
-        SELECT ?resource AS ?page ?resourceLabel AS ?label ?doc AS ?document ?docLabel AS ?documentLabel  ?docCitation AS ?citation {
+        SELECT ?resource AS ?page_id ?resourceLabel AS ?page ?doc AS ?document_id ?docLabel AS ?document  ?docCitation AS ?citation {
             ?resource ms:documentation ?doc; rdfs:label ?resourceLabel .
             ?doc rdfs:label ?docLabel ; dcterms:bibliographicCitation ?docCitation  .
         }
     } UNION {
-        SELECT ?resource AS ?page ?resourceLabel AS ?label ?doc AS ?document ?docLabel AS ?documentLabel {
+        SELECT ?resource AS ?page_id ?resourceLabel AS ?page ?doc AS ?document_id ?docLabel AS ?document {
             ?doc dcterms:references ?resource; rdfs:label ?docLabel  ; dcterms:bibliographicCitation ?docCitation.
             ?resource rdfs:label ?resourceLabel .
         }
@@ -73,12 +73,26 @@ FILTER
            ( SELECT (1)
              WHERE
                {
-                 ?page a bio:Service
+                 ?page_id a bio:Service
                }
            )
          )
       )
-} GROUP BY ?page ORDER BY ?label
+} GROUP BY ?page_id ORDER BY ?page
+EOF
+
+    self.query(query)
+  end
+
+  def self.find_all_faceted_by_topic
+    query = <<EOF
+prefix dc:  <http://purl.org/dc/elements/1.1/>
+prefix dcterms:  <http://purl.org/dc/terms/>
+prefix ms: <http://gilmere.upf.edu/ms.ttl#>
+prefix bio: <http://gilmere.upf.edu/bio.ttl#>
+SELECT ?topic_id ?topic ?doc_id ?doc ?citation
+WHERE {  ?doc_id rdfs:label ?doc ; dc:subject ?topic_id ; dcterms:bibliographicCitation ?citation.  ?topic_id rdfs:label  ?topic .}
+GROUP BY ?topic_id ORDER BY ?topic
 EOF
 
     self.query(query)
