6e4a3bb6877ad3d9675ebb0aa1e836f54bcdf5f7
Santi <santiago.bel@webfordesigners.com>
2014-01-17 17:37:36 +0100

    labelizer improved again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,12 @@ module ApplicationHelper
   def back_to_case(text)
 
     result = text
-    %w( Lmf Wsdl Rdf Owl Iula Xces Nlp Html Panacea).each do |i|
+    %w( Lmf Wsdl Rdf Owl Iula Xces Nlp Html Panacea Pdf Xml Txt Tgz Cqp).each do |i|
       result = result.gsub(Regexp.new("#{i}"), "#{i.upcase}")
     end
     {"Gr Af" => "GrAF",
      "Ms Word" => "MS Word",
+     "Po S" => "PoS",
      "Tf Idf" => "TF-IDF"}.each do |i, j|
       result = result.gsub(Regexp.new("#{i}"), "#{j}")
     end
