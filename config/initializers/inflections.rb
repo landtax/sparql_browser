898d49b091909bba053c11ae64e4afa65a4734a1
Santi <santiago.bel@webfordesigners.com>
2014-01-21 10:54:02 +0100

    pluralize exceptions in inflector

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,6 +9,10 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular 'lexicon', 'lexica'
+  inflect.irregular 'corpus', 'corpora'
+end
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
