b96c758a33f97cc0ee2050fa03ef0f27b632adb6
Marc Poch <marc.pochriera@upf.edu>
2014-10-07 10:11:58 +0200

    changing server prefixes and fixing lod sparql endpint address

--- config/initializers/sparql.rb
+++ config/initializers/sparql.rb
@@ -1,2 +1,2 @@
-$sparql = SPARQL::Client.new("http://iula02v.upf.edu/sparql")
+$sparql = SPARQL::Client.new("http://lodserver.iula.upf.edu/sparql")
 $sparql_cache = ActiveSupport::Cache::MemoryStore.new(expires_in: 5.minutes)
